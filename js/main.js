d5283e7e249f372fe17bdeac34228730174993ba
Caprtainx <benjamin.n.medina12@gmail.com>
2022-10-10 11:36:50 -0700

    moved cached elements and event listener

--- js/main.js
+++ js/main.js
@@ -6,14 +6,15 @@ let columns = 10;
 let amountOfMines = 8;
 let locationOfMines = [];
 
-let tilesClicked = 0;
+let squaresClicked = 0;
 let flagEnabled = false;
 
 let gameOver = false;
 /*----- cached elements  -----*/
+document.getElementById('mines-count').innerText = amountOfMines;
 
 /*----- event listeners -----*/
-
+document.getElementById('flag-button').addEventListener('click', setFlag);
 /*----- functions -----*/
 init();
 
@@ -22,18 +23,41 @@ function init() {
 }
 
 function gameStart() {
-    document.getElementById('mines-count').innerText = amountOfMines;
-
+    
     // generate the board
     for (let r = 0; r < rows; r++) {
         let row = [];
         for (let c = 0; c < columns; c++) {
             let square = document.createElement('div')
             square.id = r.toString() + '-' + c.toString();
+            square.addEventListener('click', clickSquare);
             document. getElementById('board').append(square);
             row.push(square);
         }
         board.push(row);
     }
+    // check if the divs of the board were made
     console.log(board);
+}
+// this allows us to toggle the flag button on and off
+function setFlag() {
+    if (flagEnabled) {
+        flagEnabled = false;
+        document.getElementById('flag-button').style.backgroundColor = 'lightgrey';
+    } else {
+        flagEnabled = true;
+        document.getElementById('flag-button').style.backgroundColor = 'darkgrey';
+    }
+}
+
+function clickSquare() {
+    // this allows us to place and remove flags as needed
+    let square = this;
+    if (flagEnabled) {
+        if (square.innerText == '') {
+            square.innerText = '🚩';
+        } else if (square.innerText == '🚩') {
+            square.innerText = '';
+        }
+    }
 }
